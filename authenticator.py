efba77286e571ab16200e4317239b6a6de0aca94
Adrien Emery <adrien.emery@gmail.com>
2016-10-22 14:19:12 -0700

    remove self authentication

--- authenticator.py
+++ authenticator.py
@@ -1,4 +1,5 @@
 import envitro
+import logging
 import requests
 
 from twisted.internet.defer import inlineCallbacks
@@ -6,9 +7,8 @@ from twisted.internet.defer import inlineCallbacks
 from autobahn.twisted.wamp import ApplicationSession, ApplicationRunner
 from autobahn.wamp.exception import ApplicationError
 
-
+logger = logging.getLogger(__name__)
 AUTH_ID = 'authenticator'
-AUTH_TICKET = envitro.str('AUTH_TICKET', '@1ox6*xba-t23l)y_&#_2#7epg-3oc&e@^kmgw7nk*e#g)5f_^')
 
 
 class AuthenticatorSession(ApplicationSession):
@@ -16,13 +16,6 @@ class AuthenticatorSession(ApplicationSession):
     def onConnect(self):
         print('Connecting to {} as {}'.format(self.config.realm, AUTH_ID))
         self.join(realm=self.config.realm, authmethods=['ticket'], authid=AUTH_ID)
-
-    def onChallenge(self, challenge):
-        if challenge.method == 'ticket':
-            print("WAMP-Ticket challenge received: {}".format(challenge))
-            return AUTH_TICKET
-        else:
-            raise Exception("Invalid authmethod {}".format(challenge.method))
 
     @inlineCallbacks
     def onJoin(self, details):
@@ -32,7 +25,7 @@ class AuthenticatorSession(ApplicationSession):
             headers = {'Authorization': 'Token {}'.format(ticket)}
             response = requests.post('http://0.0.0.0:8000/api/auth/validate-token/',
                                      headers=headers)
-            print(response)
+            logger.debug(response)
             if response.status_code == 200:
                 # TODO store roles in database for different components
                 # return default role
@@ -45,6 +38,6 @@ class AuthenticatorSession(ApplicationSession):
         # register authenticate method
         try:
             yield self.register(authenticate, 'com.auv.authenticate')
-            print("WAMP-Ticket dynamic authenticator registered!")
+            logger.info("WAMP-Ticket dynamic authenticator registered!")
         except Exception as e:
-            print("Failed to register dynamic authenticator: {0}".format(e))
+            logger.warning("Failed to register dynamic authenticator: {0}".format(e))
