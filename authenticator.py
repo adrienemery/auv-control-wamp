ca58a80fac41fe7bdea5792397782fc8d928dcd8
Adrien Emery <adrien.emery@gmail.com>
2018-08-24 18:22:50 -0700

    Use twisted logger

--- authenticator.py
+++ authenticator.py
@@ -1,20 +1,21 @@
 import os
-import logging
 import requests
 
 from twisted.internet.defer import inlineCallbacks
+from twisted.logger import Logger
 
 from autobahn.twisted.wamp import ApplicationSession, ApplicationRunner
 from autobahn.wamp.exception import ApplicationError
 
-logger = logging.getLogger(__name__)
 AUTH_ID = 'authenticator'
 TOKEN_VALIDATION_URL = os.getenv('TOKEN_VALIDATION_URL')
 
 class AuthenticatorSession(ApplicationSession):
 
+    log = Logger()
+
     def onConnect(self):
-        print('Connecting to {} as {}'.format(self.config.realm, AUTH_ID))
+        self.log.info('Connecting to f{self.config.realm} as {AUTH_ID}')
         self.join(realm=self.config.realm, authmethods=['ticket'], authid=AUTH_ID)
 
     @inlineCallbacks
@@ -23,8 +24,7 @@ class AuthenticatorSession(ApplicationSession):
         def authenticate(realm, authid, details):
             ticket = details['ticket']
             headers = {'Authorization': 'Token {}'.format(ticket)}
-            response = requests.post(TOKEN_VALIDATION_URL,
-                                     headers=headers)
+            response = requests.post(TOKEN_VALIDATION_URL, headers=headers)
             logger.debug(response)
             if response.status_code == 200:
                 # TODO store roles in database for different components
@@ -33,11 +33,11 @@ class AuthenticatorSession(ApplicationSession):
             else:
                 raise ApplicationError("com.auv.invalid_ticket",
                                        "could not authenticate session - invalid ticket "
-                                       "'{}' for principal {}".format(ticket, authid))
+                                       f"'{ticket}' for principal {authid}")
 
         # register authenticate method
         try:
             yield self.register(authenticate, 'com.auv.authenticate')
-            logger.info("WAMP-Ticket dynamic authenticator registered!")
-        except Exception as e:
-            logger.warning("Failed to register dynamic authenticator: {0}".format(e))
+            self.log.info("WAMP-Ticket dynamic authenticator registered!")
+        except Exception as exc:
+            self.log.warning(f"Failed to register dynamic authenticator: {exc}")
